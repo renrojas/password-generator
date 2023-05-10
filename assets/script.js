16d31adb4cac1d392adbdf5eca6d9935a4d7ddfa
renrojas <karensadithrojas@gmail.com>
2023-05-10 14:25:14 -0600

    removed console.logs and finalized javascript

--- assets/script.js
+++ assets/script.js
@@ -5,86 +5,66 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var generatedPassword = "";
   var passwordLength = 0;
-  // loop to prompt and validate, i don't know how many times - while loop
+  // loop to prompt and validate: while loop
   while(passwordLength <8 || passwordLength > 128) {
     passwordLength = prompt ("Please set the length between 8 to 128 characters, inclusive")
- console.log(passwordLength)
     if (passwordLength >= 8 && passwordLength <=128) {
     } else if(passwordLength!==null){
     alert("Please select a valid number equal or greater than 8 and less than or equal to 128")
     } else {break}
   }
+  //ends loop if user clicks 'Cancel'
   if (passwordLength===null) {
     return ""
   }
-  console.log(passwordLength)
 
   var lowerCase = false
-
   var upperCase = false;
-
   var numbers = false;
-
   var specialChars = false;
 
+  //Asking user what type of characters to include
   while((lowerCase||upperCase||numbers||specialChars)== false) {
     lowerCase = window.confirm("Would you like to include lowercase letters?");
-    console.log("lowercase?", lowerCase);
-  
     upperCase = window.confirm("Would you like to include uppercase letters?");
-    console.log("uppercase?", upperCase);
-  
     numbers = window.confirm("Would you like to include numbers?");
-    console.log("numbers?", numbers);
-  
     specialChars = window.confirm("Would you like to include special characters?");
-    console.log("special characters?", specialChars);
-  
+    
+    //Validate that at least one type of characters has been selected
     if ((lowerCase||upperCase||numbers||specialChars)== false) {
       alert("Please select atleast one of the categories.")
-  
-    }
-  }
+    }}
 
+//characters set: lowercase, uppercase, numbers, and special characters    
 var lower = "abcdefghijklmnopqrstuvwxyz"
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numberOptions = "1234567890"
 var special = "~!@#$%^&*()_+-={}|[]\:;'<>,./?'"
 
+//Adds characters based on user preferences
 var userString = ""
-
   if (lowerCase) {
-    console.log("before", userString)
     userString = userString + lower
-    console.log("after",userString)
   }
   if (upperCase) {
-    console.log("before", userString)
     userString = userString + upper
-    console.log("after", userString)
   }
   if (numbers) {
-    console.log("before", userString)
     userString = userString + numberOptions
-    console.log("after", userString)
   }
   if (specialChars) {
-    console.log("before", userString)
     userString = userString + special
   }
 
-
+//for loop to randomnize string based on password length
 for (var i = 0; i<passwordLength; i++){
   var random = Math.floor(Math.random() * userString.length);
-  console.log(userString[random])
+
   generatedPassword=generatedPassword + userString[random]
 }
-
   return generatedPassword
 }
 
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
 
@@ -92,10 +72,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+//Activates when user clicks on button
 generateBtn.addEventListener("click", writePassword);
 
 
